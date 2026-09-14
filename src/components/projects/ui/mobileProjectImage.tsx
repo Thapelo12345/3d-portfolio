37538b7212fedaab7ebce9770df4a9aa73ad5c97
Thapelo12345 <79000570+Thapelo12345@users.noreply.github.com>
2026-09-14 12:49:00 +0200

    adding scale

--- src/components/projects/ui/mobileProjectImage.tsx
+++ src/components/projects/ui/mobileProjectImage.tsx
@@ -5,7 +5,7 @@ type PROP = {
 
 export default function MobileProjectImage({ imageUrls, leftPosition }: PROP) {
   return (
-    <div className={`left-[${leftPosition}%]absolute top-0 hover:scale-125 transition-all duration-500 bg-transparent w-[59%] h-full md:h-80 rounded-lg`}>
+    <div className={`left-[${leftPosition}%]absolute top-0 hover:scale-105 transition-all duration-500 bg-transparent w-[59%] h-full md:h-80 rounded-lg`}>
       <img className="w-50 h-full" src={imageUrls} alt="Project image" />
     </div>
   );
